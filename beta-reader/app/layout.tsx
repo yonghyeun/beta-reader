941dc21e33001044484c9ea7dad7b99393651a5c
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2025-04-08 21:59:01 +0900

    [Feat] 디자인 시스템 컬러 및 폰트 설정 (#5)
    * feat[#4] : color 시스템 생성
    
    * feat[#4] : pretandard 폰트 설정
    
    * feat[#4] : border-radius 값 추가
    
    * feat[#4] : 폰트 유틸리티 클래스 생성

--- beta-reader/app/layout.tsx
+++ beta-reader/app/layout.tsx
@@ -1,14 +1,41 @@
 import "./globals.css";
+import localFont from "next/font/local";
+
+const pretendard = localFont({
+  src: [
+    {
+      path: "./Pretendard-Regular.woff",
+      weight: "400",
+      style: "normal"
+    },
+    {
+      path: "./Pretendard-Medium.woff",
+      weight: "500",
+      style: "normal"
+    },
+    {
+      path: "./Pretendard-SemiBold.woff",
+      weight: "600",
+      style: "normal"
+    },
+    {
+      path: "./Pretendard-Bold.woff",
+      weight: "700",
+      style: "normal"
+    }
+  ],
+  variable: "--font-pretendard",
+  display: "swap"
+});
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+  children
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
+    <html lang="en" className={pretendard.variable}>
       <body>{children}</body>
     </html>
   );
 }
-
