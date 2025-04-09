34cc400aeb7b1908c20195d82f8d897d5f6ce821
yonghyeun <ttddcc119@naver.com>
2025-04-09 14:26:50 +0900

    refactor[#9] : font 가져오는 로직 shared 에 추상화

--- beta-reader/app/layout.tsx
+++ beta-reader/app/layout.tsx
@@ -1,32 +1,5 @@
 import "./globals.css";
-import localFont from "next/font/local";
-
-const pretendard = localFont({
-  src: [
-    {
-      path: "../public/fonts/Pretendard-Regular.woff",
-      weight: "400",
-      style: "normal"
-    },
-    {
-      path: "../public/fonts/Pretendard-Medium.woff",
-      weight: "500",
-      style: "normal"
-    },
-    {
-      path: "../public/fonts/Pretendard-SemiBold.woff",
-      weight: "600",
-      style: "normal"
-    },
-    {
-      path: "../public/fonts/Pretendard-Bold.woff",
-      weight: "700",
-      style: "normal"
-    }
-  ],
-  variable: "--font-pretendard",
-  display: "swap"
-});
+import { pretendard } from "@/src/shared/config/fonts";
 
 export default function RootLayout({
   children
@@ -34,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={pretendard.variable}>
+    <html lang="ko" className={pretendard.variable}>
       <body>{children}</body>
     </html>
   );
