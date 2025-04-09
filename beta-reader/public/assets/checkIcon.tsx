75e1ae1b5237aa70bba7323c031215fc3c2bd9c6
yonghyeun <ttddcc119@naver.com>
2025-04-09 15:17:00 +0900

    feat[#9] : svg icon camelCase 로 변경

--- beta-reader/public/assets/checkIcon.tsx
+++ beta-reader/public/assets/checkIcon.tsx
@@ -9,9 +9,9 @@ export const CheckIcon = () => (
     <path
       d="M0.714355 3.39258L3.79128 7.14258L8.71436 1.14258"
       stroke="#1E1F24"
-      stroke-width="1.4"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.4"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
