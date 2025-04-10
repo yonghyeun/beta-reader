27659c8070c249d87d3840b2ef5b3b496d99d89a
yonghyeun <ttddcc119@naver.com>
2025-04-10 11:43:48 +0900

    faet[#17] : Button 컴포넌트 cva 적용

--- beta-reader/src/shared/ui/Button.tsx
+++ beta-reader/src/shared/ui/Button.tsx
@@ -1,29 +1,36 @@
+import { type VariantProps, cva } from "class-variance-authority";
 import React from "react";
 
-const buttonSize = {
-  sm: "px-3 py-1.5 text-caption-1-medium gap-1", // 12px, 6px, gap 4px
-  md: "px-5 py-2 text-caption-1-medium gap-1", // 20px, 8px, gap 4px
-  lg: "px-5 py-3 text-body-1-semibold gap-1.5" // 20px, 12px, gap 6px
-} as const;
+const buttonVariants = cva("flex items-center justify-center", {
+  variants: {
+    size: {
+      sm: "px-3 py-1.5 text-caption-1-medium gap-1", // 12px, 6px, gap 4px
+      md: "px-5 py-2 text-caption-1-medium gap-1", // 20px, 8px, gap 4px
+      lg: "px-5 py-3 text-body-1-semibold gap-1.5" // 20px, 12px, gap 6px
+    },
+    variant: {
+      primary:
+        "bg-primary-400 text-secondary-white hover:bg-primary-500 disabled:bg-secondary-700 disabled:text-secondary-300",
+      secondary:
+        "bg-secondary-700 text-secondary-white hover:bg-secondary-600 disabled:bg-secondary-700 disabled:text-secondary-300",
+      white:
+        "bg-secondary-white text-secondary-black hover:bg-secondary-200 disabled:bg-secondary-700 disabled:text-secondary-300"
+    },
+    rounded: {
+      md: "rounded-xl", // 12px
+      full: "rounded-full"
+    }
+  },
+  defaultVariants: {
+    size: "md",
+    variant: "primary",
+    rounded: "md"
+  }
+});
 
-const buttonColors = {
-  primary:
-    "bg-primary-400 text-secondary-white hover:bg-primary-500  disabled:bg-secondary-700 disabled:text-secondary-300 ",
-  secondary:
-    "bg-secondary-700 text-secondary-white hover:bg-secondary-600  disabled:bg-secondary-700 disabled:text-secondary-300 ",
-  white:
-    "bg-secondary-white text-secondary-black hover:bg-secondary-200 disabled:bg-secondary-700 disabled:text-secondary-300"
-} as const;
-
-const buttonRadius = {
-  md: "rounded-xl", // 12px
-  full: "rounded-full"
-} as const;
-
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  size: keyof typeof buttonSize;
-  variant: keyof typeof buttonColors;
-  rounded: keyof typeof buttonRadius;
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
   className?: string;
 }
 
@@ -36,7 +43,7 @@ export const Button: React.FC<ButtonProps> = ({
   ...props
 }) => (
   <button
-    className={`flex items-center justify-center ${buttonSize[size]} ${buttonColors[variant]} ${buttonRadius[rounded]} ${className}`}
+    className={buttonVariants({ size, variant, rounded, className })}
     {...props}
   >
     {children}
