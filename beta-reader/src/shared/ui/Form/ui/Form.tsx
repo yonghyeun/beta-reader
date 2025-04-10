fc273a3b99973bc131ea5d1326d7222002e831d5
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2025-04-10 18:37:17 +0900

    [Feat] Form 컨테이너 컴포넌트 생성 (#24)
    * feat[#23] : Form 컨테이너 컴포넌트 생성
    
    * refactor[#23] : Default 컴포넌트 수정

--- beta-reader/src/shared/ui/Form/ui/Form.tsx
+++ beta-reader/src/shared/ui/Form/ui/Form.tsx
@@ -1,1 +1,33 @@
-export const Form = () => {};
+interface FormProps extends React.HTMLProps<HTMLFormElement> {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Wrapper: React.FC<FormProps> = ({
+  children,
+  className = "",
+  ...props
+}) => {
+  return (
+    <form className={className} {...props}>
+      {children}
+    </form>
+  );
+};
+
+interface ContainerProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Container: React.FC<ContainerProps> = ({ children, className }) => {
+  return (
+    <section
+      className={`bg-secondary-900 rounded-[1.25rem] px-7 py-6 ${className}`}
+    >
+      {children}
+    </section>
+  );
+};
+
+export const Form = Object.assign(Wrapper, { Container });
