7ec8b2de1f61e8e2cd34501afb3a3ca9fe9cddc2
yonghyeun <ttddcc119@naver.com>
2025-04-19 23:47:53 +0900

    feat[#43] : dropdown 컴포넌트 수정
    - varaint 추가하여 selector 스타일링

--- beta-reader/src/shared/ui/Dropdown/ui/Dropdown.tsx
+++ beta-reader/src/shared/ui/Dropdown/ui/Dropdown.tsx
@@ -1,55 +1,81 @@
-import { useMemo, useRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { useRef } from "react";
 
-import { useDropdown } from "../lib";
-import { DropdownContext, useDropdownContext } from "../model";
-import { DownLargeIcon, UpLargeIcon } from "@/src/shared/assets";
+import { DropdownContext, useDropdown, useDropdownContext } from "../lib";
+import {
+  DownLargeIcon,
+  DownSmallIcon,
+  UpLargeIcon,
+  UpSmallIcon
+} from "@/src/shared/assets";
 
-interface DropDownProps {
+const selectorVariant = cva("flex items-center justify-between", {
+  variants: {
+    variant: {
+      default: "",
+      padded:
+        "px-5 py-6 rounded-[1.25rem] bg-secondary-900 rounded-[1.25rem] min-w-[15.5rem]"
+    }
+  }
+});
+
+interface DropDownProps extends VariantProps<typeof selectorVariant> {
   initialValue?: string;
   children: React.ReactNode;
   onDropdownChange?: (value: string) => void;
+  className?: string;
 }
+
+const dropdownIcons = (variant: "default" | "padded") => (status: boolean) => {
+  const dropdownIcons = {
+    default: {
+      open: <UpSmallIcon />,
+      close: <DownSmallIcon />
+    },
+    padded: {
+      open: <UpLargeIcon />,
+      close: <DownLargeIcon />
+    }
+  };
+  return dropdownIcons[variant][status ? "open" : "close"];
+};
 
 const DropdownContainer: React.FC<DropDownProps> = ({
   children,
   onDropdownChange,
-  initialValue = ""
+  initialValue = "",
+  className,
+  ...props
 }) => {
   const dropdownRef = useRef<HTMLDivElement>(null);
-  const { value, closeDropDown, isOpen, openDropdown } = useDropdown(
+  const dropdonwLogic = useDropdown(
     dropdownRef,
     initialValue,
     onDropdownChange
   );
+  const getDropdownIcons = dropdownIcons(props.variant || "default");
 
-  const dropdownId = useMemo(
-    () => `dropdown-${Math.random().toString(36).slice(2, 9)}`,
-    []
-  );
+  const { isOpen, dropdownId, closeDropDown, openDropdown, value } =
+    dropdonwLogic;
 
   return (
-    <DropdownContext
-      value={{
-        closeDropDown
-      }}
-    >
+    <DropdownContext value={{ ...dropdonwLogic }}>
       <div ref={dropdownRef} className="relative">
+        {/* Dropdown Selector */}
         <button
           aria-haspopup="listbox"
           aria-expanded={isOpen}
           aria-controls={dropdownId}
           onClick={() => (isOpen ? closeDropDown(value) : openDropdown())}
-          className="text-secondary-white bg-secondary-900 flex min-w-72 items-center justify-between rounded-[1.25rem] px-5 py-6"
+          className={selectorVariant({ ...props, className })}
         >
           <span>{value}</span>
-          <span aria-hidden="true">
-            {isOpen ? <UpLargeIcon /> : <DownLargeIcon />}
-          </span>
+          <span aria-hidden="true">{getDropdownIcons(isOpen)}</span>
         </button>
-
-        {isOpen && (
+        {/* Dropdown Items */}
+        {dropdonwLogic.isOpen && (
           <ul
-            id={dropdownId}
+            id={dropdonwLogic.dropdownId}
             role="listbox"
             className="text-secondary-white bg-secondary-800 border-secondary-500 absolute top-full mt-2 flex min-w-72 flex-col gap-1 rounded-[1.25rem] border p-5"
           >
