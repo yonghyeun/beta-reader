6f849d28e3ce4864cb867820e5e0762db3e901d5
yonghyeun <ttddcc119@naver.com>
2025-04-21 15:29:41 +0900

    feat[#45] : NovelMetaCard 컴포넌트 생성

---
+++ beta-reader/src/entities/novel/ui/NovelMetaCard.tsx
@@ -0,0 +1,60 @@
+import { BookIcon, MeatballIcon } from "@/src/shared/assets";
+
+interface NovelMetaCardProps {
+  novelTitle: string;
+  manuscriptCount: number;
+  commentsCount: number;
+  lastUpdated: string;
+  episodes: { title: string; lastUpdated: string }[];
+}
+
+export const NovelMetaCard: React.FC<NovelMetaCardProps> = ({
+  novelTitle,
+  manuscriptCount,
+  commentsCount,
+  lastUpdated,
+  episodes
+}) => {
+  return (
+    <section
+      aria-labelledby="novel-title"
+      className="bg-secondary-900 border-secondary-700 flex flex-col gap-5 rounded-2xl border px-[1.625rem] py-5"
+    >
+      <header className="flex flex-col gap-2">
+        <div className="flex justify-between">
+          <BookIcon aria-hidden="true" />
+          <button
+            aria-label="소설 옵션 메뉴"
+            className="flex items-center justify-center"
+          >
+            <MeatballIcon aria-hidden="true" />
+          </button>
+        </div>
+        <h3 id="novel-title" className="text-body-2-regular line-clamp-1">
+          {novelTitle}
+        </h3>
+        <div className="text-secondary-200 text-caption-2-regular flex gap-2.5">
+          <p>{manuscriptCount}개의 원고지</p>
+          <p>{commentsCount}회의 댓글</p>
+          <p className="text-caption-3-regular text-primary-200">
+            {lastUpdated} 마지막 편집
+          </p>
+        </div>
+      </header>
+
+      <ul className="grid grid-cols-2 gap-6">
+        {episodes.map((episode) => (
+          <li
+            key={episode.title}
+            className="bg-secondary-800 border-secondary-600 flex flex-col gap-2 rounded-2xl border px-[1.625rem] py-6"
+          >
+            <p className="text-body-2-medium line-clamp-1">{episode.title}</p>
+            <p className="caption-3-regular text-secondary-300">
+              {episode.lastUpdated}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
